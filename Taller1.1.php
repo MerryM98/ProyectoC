f5ea581c246c2dd7f328f52ff7d9b843584cd061
MerryM98 <61071116+MerryM98@users.noreply.github.com>
2020-02-22 17:16:48 -0500

    punto1

---
+++ Taller1.1.php
@@ -0,0 +1,38 @@
+<?php
+echo("Taller 1 Merilian Mejia Palacio <br>");
+echo("Punto 1<br>");
+
+$numero1=40;
+$numero2=50;
+
+
+$suma=$numero1+$numero2;
+$resta=$numero1-$numero2;
+$multiplicacion=$numero1*$numero2;
+$division=$numero1/$numero2;
+
+echo("<br>La suma de:".$numero1."+".$numero2."=".$suma);
+
+echo("<br>La resta de:".$numero1."-".$numero2."=".$resta);
+
+echo("<br>La Multiplicación de:".$numero1."*".$numero2."=".$multiplicacion); 
+
+echo("<br>La división de:".$numero1."/".$numero2."=".$division);
+
+
+echo("pregunta 2 <br>");
+
+$peso=85;
+$altura=1.75;
+$imc=$peso/($altura*$altura);
+
+if(imc<=18,4)
+{
+    echo("insuficiente peso:")
+}
+else
+if(imc)
+
+
+
+?>
